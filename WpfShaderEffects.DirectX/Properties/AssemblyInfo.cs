27c275bf0d5981b7768bb25836443db2b2057241
marten_range <2491891+mrange@users.noreply.github.com>
2009-07-28 08:46:48 +0000

    Added build configuration to build signed dlls. Added key sign

--- WpfShaderEffects.DirectX/Properties/AssemblyInfo.cs
+++ WpfShaderEffects.DirectX/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WpfShaderEffects.DirectX")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool to compile pixel shaders")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfShaderEffects.DirectX")]
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
